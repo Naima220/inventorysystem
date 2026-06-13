13abbb68931ae8d7687e7d1fc5e09bab890e6486
Naima119 <nxj2233@gmail.com>
2026-06-13 13:09:12 +0300

    Remove duplicate jQuery that breaks sidebar

--- resources/views/admin/delivered_orders.blade.php
+++ resources/views/admin/delivered_orders.blade.php
@@ -55,7 +55,7 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.bootstrap4.min.css"/>
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap4.min.css"/>
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
